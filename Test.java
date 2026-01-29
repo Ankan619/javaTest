1af8b3d7dfc04139c67e527c383769424d032cab
transora <transoraindia@gmail.com>
2026-01-29 11:02:28 +0530

    Update Test.java
    change the value of x anf y

--- Test.java
+++ Test.java
@@ -31,12 +31,12 @@ public class Test {
     // Main method
     public static void main(String[] args) {
         int x = 589;
-        int y = 10;
+        int y = 1000;
 
-        add(x, y);
-        sub(x, y);
-        mul(x, y);
-        div(x, y);
+        add(y, x);
+        sub(y, x);
+        mul(y, x);
+        div(y, x);
     }
 }
 
